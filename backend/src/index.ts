f402401e4af9dc1bf704ab9f213521a88db7527c
ronbarrantes <ronbarrantes@gmail.com>
2021-08-24 22:40:09 -0700

    Add a port for index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,10 @@
-const hello = (text='World'): string => `Hello, ${text}!`
-console.log(hello())
+import express, { Request, Response } from 'express'
+const PORT = 3000
+
+const app = express()
+
+app.get('/', (_req: Request, res: Response) => {
+    res.send('hello')
+})
+
+app.listen(PORT, () => console.log(`SERVER UP @ localhost:${PORT}`))
