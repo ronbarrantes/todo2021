b31604a9194f84e1b0a98f3cca31839dd9194f15
ronbarrantes <ronbarrantes@gmail.com>
2021-08-25 13:22:49 -0700

    Implement start

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,2 @@
-import express, { Request, Response } from 'express'
-import Todo from './routes/Todo'
-
-const PORT = 3000
-
-// fake state
-const app = express()
-app.use(express.json())
-
-app.use(Todo)
-
-app.get('/', (_req: Request, res: Response) => {
-    return res.send('hello')
-})
-
-app.listen(PORT, () => console.log(`SERVER UP @ localhost:${PORT}`))
+import * as server from './lib/server'
+server.start()
