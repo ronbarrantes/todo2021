7c9343af9f736349086a7348892b7a46cb0e3d0b
ronbarrantes <ronbarrantes@gmail.com>
2021-08-28 01:07:36 -0700

    Modify the config file

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,5 +1,6 @@
-export const appName = 'Todo'
+export const appName = 'todoApp'
 export const isProduction = false
 export const port = 3000
-export const mongodb = { url: `mongodb://localhost:27017/${appName}` }
+export const mongoosePort = 27017
+export const mongodb = { url: `mongodb://localhost/${appName}` }
 export const apiURL = `http://localhost:${port}`
