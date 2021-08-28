92e0e916c0ebf47abd6e79406d621b7515db8ff1
ronbarrantes <ronbarrantes@gmail.com>
2021-08-27 17:20:19 -0700

    Change name of app

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,4 +1,4 @@
-export const dbName = 'todo2021'
+export const dbName = 'todoApp2021'
 export const isProduction = false
 export const port = 3000
 export const mongodb = { url: `mongodb://localhost:27017/${dbName}` }
