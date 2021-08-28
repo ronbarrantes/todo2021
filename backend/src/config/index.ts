7881e09192b44919046b570fdf44b692e1acb2e6
ronbarrantes <ronbarrantes@gmail.com>
2021-08-27 17:35:58 -0700

    Change the name of the app

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,4 +1,4 @@
-export const appName = 'TodoApp'
+export const appName = 'todoApp'
 export const isProduction = false
 export const port = 3000
 export const mongodb = { url: `mongodb://localhost:27017/${appName}` }
