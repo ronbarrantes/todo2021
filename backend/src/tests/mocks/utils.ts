e4be360f5fdff588e9a4f95150bdf4e20c2f5305
ronbarrantes <ronbarrantes@gmail.com>
2021-08-28 18:44:26 -0700

    Modify makeRandomWord()

--- backend/src/tests/mocks/utils.ts
+++ backend/src/tests/mocks/utils.ts
@@ -5,9 +5,8 @@ import { lorem } from 'faker'
  * @param numberOfWords the number of words to input
  * @returns string
  */
-export const makeRandomWords = (numberOfWords = 0): string => {
-    const wordAmount = numberOfWords >= 0 ? numberOfWords : Math.floor(Math.random()* 5) + 1
-
-    const result = lorem.words(wordAmount)
+export const makeRandomWords = (numberOfWords?: number): string => {
+    numberOfWords = numberOfWords || Math.floor(Math.random()* 5) + 1
+    const result = lorem.words(numberOfWords)
     return result
 }
