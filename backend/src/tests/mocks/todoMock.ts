c2dbe582086ca4fbf8016e534f07fa5e3160c3fa
ronbarrantes <ronbarrantes@gmail.com>
2021-08-28 19:12:19 -0700

    Fix todoMocks

--- backend/src/tests/mocks/todoMock.ts
+++ backend/src/tests/mocks/todoMock.ts
@@ -1,22 +1,20 @@
 import { makeRandomWords } from './utils'
 import TodoServices from '../../services/TodoServices'
 import { Document } from 'mongoose'
+import { TodoModel } from '../../models/TodoModel'
 
 type Create = (text?: string) => Promise<Document>
 type CreateMany = (num?: number) => Promise<Document[]>
 
-// TODO: mockOne
 export const create: Create = async (text) => {
     text = text || makeRandomWords()
-
-    console.log('TEXT ==>', text)
     return await TodoServices.create({ task: text })
 }
 
-// TODO: mockMany
 export const createMany: CreateMany = async (num = 5) => {
-    const result = await Promise.all(new Array(num).map(async () => await create()))
+    const result = await Promise.all(new Array(num).fill({}).map(async () => await create()))
+    console.log(result)
     return result
 }
 
-// TODO: mockRemove
+export const remove = async (): Promise<void> => TodoModel.deleteMany({})
