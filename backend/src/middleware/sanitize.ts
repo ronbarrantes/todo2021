cb1280d70e9d07505868ac6a320a634b2c468b68
ronbarrantes <ronbarrantes@gmail.com>
2021-08-30 15:15:52 -0700

    Update sanitize

--- backend/src/middleware/sanitize.ts
+++ backend/src/middleware/sanitize.ts
@@ -1,8 +1,15 @@
 import { Request, Response, NextFunction } from 'express'
+import { ITodo } from '../models/TodoModel'
 
 export const sanitizeBody = (req: Request, _res: Response, next: NextFunction): any => {
-    if(req.body._id)
-        delete req.body._id
+    const body: ITodo = req.body
 
+    if(body._id)
+        delete body._id
+
+    if(body.createdAt)
+        delete body.createdAt
+
+    req.body = body
     next()
 }
