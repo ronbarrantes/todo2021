0af935f674acecf9151bdb9ee08c4d7384a66c78
ronbarrantes <ronbarrantes@gmail.com>
2021-09-04 16:20:32 -0700

    Get my file running at least

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,121 +1,38 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-require('dotenv').config()
-
-const { EnvironmentPlugin } = require('webpack')
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-// const CleanWebpackPlugin = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-// const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-
-const production = process.env.NODE_ENV === 'production'
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const plugins = [
-    // new CleanWebpackPlugin(),
-
+    new CopyWebpackPlugin({ patterns: [{ from: 'public', to: 'public' }] }),
     new HtmlWebpackPlugin({
         title: `Todo Site`,
         meta: {
             viewport: `width=device-width, initial-scale=1, shrink-to-fit=no`,
         },
-        favicon: './src/assets/img/favicon.png',
+        favicon: './public/assets/favicon.png',
     }),
-
-    new EnvironmentPlugin({
-        NODE_ENV: process.env.NODE_ENV,
-        ASSETS_PATH: process.env.ASSETS_PATH,
-        API_URL: process.env.API_URL,
-    }),
-
-    new MiniCssExtractPlugin({
-        filename: !production ? '[name].css' : '[name].[hash].css',
-        chunkFilename: !production ? '[id].css' : '[id].[hash].css',
-    }),
-
-//   new OptimizeCSSAssetsPlugin({}),
 ]
-
-const optimization = {
-    runtimeChunk: 'single',
-    splitChunks: {
-        cacheGroups: {
-            vendor: {
-                test: /[\\/]node_modules[\\/]/,
-                name: 'vendors',
-                chunks: 'all',
-            },
-        },
-    },
-}
-
-// if (production) {
-//   optimization.minimizer = [new UglifyJsPlugin()]
-// }
 
 module.exports = {
     plugins,
-    optimization,
-    devtool: production ? undefined : 'source-map',
-    mode: process.env.NODE_ENV,
-    entry: `${__dirname}/src/main.js`,
-
-    output: {
-        filename: 'bundle.[hash].js',
-        path: `${__dirname}/dist`,
-        publicPath: process.env.CDN_URL,
-    },
-
-    devServer: {
-        historyApiFallback: true,
-    },
-
+    watch: true,
+    mode: 'development',
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.tsx?$/,
+                use: 'ts-loader',
                 exclude: /node_modules/,
-                loader: ['babel-loader'],
-            },
-
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    !production ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-
-            {
-                test: /\.(woff|woff2|ttf|eot).*/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 10000,
-                            name: 'font/[name].[hash].[ext]',
-                        },
-                    },
-                ],
-            },
-
-            {
-                test: /\.(jpg|gif|png|svg)$/,
-                exclude: /\.icon\.svg$/,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 10000,
-                        name: 'image/[name].[hash].[ext]',
-                    },
-                }],
-            },
-
-            {
-                test: /\.icon\.svg$/,
-                loader: 'raw-loader',
             },
         ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
     },
 }
