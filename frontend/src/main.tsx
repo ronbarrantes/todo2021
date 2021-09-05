38e564b8ac9a6694d0f5092245a4f23cb498fd2f
ronbarrantes <ronbarrantes@gmail.com>
2021-09-05 09:51:09 -0700

    Move the App to it's own file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import App from './components/App'
+
 const container = document.createElement('div')
 container.className = 'root'
 document.body.appendChild(container)
-
-const App = () => (<div>Hello</div>)
 
 ReactDOM.render(<App />, container)
