be78a0dde0081075d0522e535377ea92164ce1f2
ronbarrantes <ronbarrantes@gmail.com>
2021-09-05 10:06:19 -0700

    Update eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -21,40 +21,40 @@ module.exports = {
         '@typescript-eslint',
     ],
     rules: {
+        '@typescript-eslint/no-use-before-define': ['error'],
         semi: ['error', 'never'],
         quotes: ['warn', 'single', { allowTemplateLiterals: true }],
         indent: ['error', 4, { SwitchCase: 1, ignoreComments: true }],
-        'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+        'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts', '.js', '.jsx'] }],
         'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': ['error'],
-
-    /*
-    // '@typescript-eslint/rule-name': 'error',
-    // 'jsx-quotes': ['error', 'prefer-double'],
-    'brace-style': ['error', '1tbs'],
-    // 'no-multi-spaces': 'warn',
-    'comma-spacing': ['warn', { before: false, after: true }],
-    // 'require-atomic-updates': 'error',
-    'no-trailing-spaces': 'error',
-    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 1 }],
-    'comma-dangle': ['error', 'always-multiline'],
-    'object-curly-spacing': ['warn', 'always'],
-    'no-console': 'off',
-    // "no-console": [2, "error"],
-    // 'no-unused-vars': 'off',
-
-    'switch-colon-spacing': ['error', { after: true, before: false }],
-
-    'space-in-parens': ['warn', 'never'],
-    'new-cap': ['error', { capIsNewExceptions: ['Record', 'Router'] }],
-    // camelcase: ['warn'],
-    'no-whitespace-before-property': 2,
-    'arrow-spacing': 'error',
-    */
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+        'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 1 }],
+        'comma-dangle': ['error', 'always-multiline'],
+        'object-curly-spacing': ['warn', 'always'],
+        'arrow-spacing': 'error',
+        'brace-style': ['error', '1tbs'],
+        'space-in-parens': ['warn', 'never'],
+        'no-whitespace-before-property': 2,
+        'jsx-quotes': ['error', 'prefer-double'],
+        'switch-colon-spacing': ['error', { after: true, before: false }],
     },
     settings: {
         react: {
             version: 'detect',
+        },
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
         },
     },
 }
