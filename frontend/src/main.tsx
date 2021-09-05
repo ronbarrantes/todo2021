c106fdff13570594d80673d6f9119920fe8d5e81
ronbarrantes <ronbarrantes@gmail.com>
2021-09-05 12:30:07 -0700

    Add me some notes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 
 import App from './components/App'
 
+// TODO: login route
+// TODO: board
+// TODO: todo items
+
 const container = document.createElement('div')
 container.className = 'root'
 document.body.appendChild(container)
